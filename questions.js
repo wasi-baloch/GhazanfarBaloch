7281a777ea1359609a7928b5ddc46b1248528906
wasi-baloch <wiser84@gmail.com>
2020-08-30 11:17:06 +0500

    comit

---
+++ questions.js
@@ -0,0 +1,67 @@
+var questionsArray = [
+    { 
+        question: " If a body moves back and forth or to and fro about a point, then it is said to be?",
+        answer: "vibrating",
+        options: ["consistency", "vibrating", "rotating", "displacing"]
+        },
+        
+        {
+         question: "In SHM of a simple pendulum, the component of weight which is directed towards mean position is?",
+        answer: "mg sinθ",
+        options: ["mg cosθ", "mg sinθ", "mg tanθ", "none of above"]
+        },
+        
+
+
+
+    {
+        question: "Full Form of RAM is?",
+        answer: "random access memory",
+        options: [
+            "random access memory",
+            "random actual memory",
+            "right access memory",
+            "none of the above",
+        ]
+    },
+    {
+        question: "Full Form of CPU is?",
+        answer: "central processing unit",
+        options: [
+            "central program unit",
+            "central processing unit",
+            "control panel unit",
+            "none of the above",
+        ]
+    },
+    // {
+    //     question: "Full Form of E-MAIL is?",
+    //     answer: "electronic mail",
+    //     options: [
+    //         "electric mail",
+    //         "easy mail",
+    //         "electronic mail",
+    //         "none of the above",
+    //     ]
+    // },
+    // {
+    //     question: "Full Form of LCD is?",
+    //     answer: "liquid crystal display",
+    //     options: [
+    //         "liquid crystal display",
+    //         "light crystal display",
+    //         "liquid cold display",
+    //         "none of the above",
+    //     ]
+    // },
+    // {
+    //     question: "Full Form of SEO is?",
+    //     answer: "none of the above",
+    //     options: [
+    //         "safe engine optimization",
+    //         "secure engine optimization",
+    //         "secret engine optimization",
+    //         "none of the above",
+    //     ]
+    // },
+];
